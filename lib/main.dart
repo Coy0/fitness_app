a72c4d5318ca03f8d80212e8bb93d32ba7485f46
Coy <ca0002@students.inghamisd.org>
2025-03-05 09:50:19 -0500

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,410 @@
+// ignore_for_file: library_private_types_in_public_api
+
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() { // Main Function
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => MyAppState(),
+      child: MaterialApp(
+        title: 'Namer App',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(
+              seedColor: const Color.fromARGB(255, 199, 115, 119)), // Seed Color
+          primaryColor: const Color.fromARGB(255, 199, 115, 119), // Primary Color
+          primaryColorLight: const Color.fromARGB(255, 199, 115, 119), // Primary Light Color
+          primaryColorDark: const Color.fromARGB(255, 20, 20, 20), // Primary Dark Color
+        ),
+        home: MyHomePage(),
+      ),
+    );
+  }
+}
+
+class MyAppState extends ChangeNotifier {
+  var current = WordPair.random();
+  var favorites = <WordPair>[];
+
+  void getNext() {
+    current = WordPair.random();
+    notifyListeners();
+  }
+
+  void toggleFavorite() {
+    if (favorites.contains(current)) {
+      favorites.remove(current);
+    } else {
+      favorites.add(current);
+    }
+    notifyListeners();
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    Widget page;
+    switch (selectedIndex) {
+      case 0:
+        page = TestLogin();
+
+      case 1:
+        page = HomePage();
+
+      case 2:
+        page = FavoritesPage();
+
+      case 3:
+        page = TestMusic();
+
+      default:
+        throw UnimplementedError('no widget for $selectedIndex');
+    }
+
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return Scaffold(
+          body: Row(
+            children: [
+              SafeArea(
+                child: NavigationRail(
+                  extended: constraints.maxWidth >= 750,
+                  destinations: [
+                    NavigationRailDestination(
+                      icon: Icon(Icons.login),
+                      label: Text('Login'),
+                    ),
+                    NavigationRailDestination(
+                      icon: Icon(Icons.home),
+                      label: Text('Home'),
+                    ),
+                    NavigationRailDestination(
+                      icon: Icon(Icons.favorite),
+                      label: Text('Favorites'),
+                    ),
+                    NavigationRailDestination(
+                      icon: Icon(Icons.music_note),
+                      label: Text('Music'),
+                    )
+                  ],
+                  selectedIndex: selectedIndex,
+                  onDestinationSelected: (value) {
+                    setState(() {
+                      selectedIndex = value;
+                    });
+                  },
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  color: Theme.of(context).colorScheme.primaryContainer,
+                  child: page,
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
+
+class TestLogin extends StatefulWidget {
+  @override
+  _TestLoginState createState() => _TestLoginState();
+}
+
+class _TestLoginState extends State<TestLogin> {
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  String _username = '';
+  String _password = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+              // App Logo
+              width: 75,
+              height: 75,
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: NetworkImage(
+                        'https://lh3.googleusercontent.com/a/AAcHTte9-uacFwE0x1rKXfsWlhp9ePVSORbZblEPIAbz7V9P6PI=s96-c'), // Test Img
+                    fit: BoxFit.cover,
+                  ),
+                  borderRadius: BorderRadius.circular(200),
+                  border: Border.all(
+                    width: 1000,
+                  ))),
+          // App Logo
+
+          SizedBox(height: 20),
+
+          Text(
+            'Sign in to [App_Name]',
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.normal),
+          ),
+
+          SizedBox(height: 20),
+
+          Container(
+            width: 350,
+            height: 230,
+            decoration: BoxDecoration(
+              border: Border.all(
+                width: 1,
+              ),
+              borderRadius: BorderRadius.circular(12),
+            ),
+            // Main Outside Box
+
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 25, vertical: 16),
+                    child: TextField(
+                      controller: _usernameController,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Enter your username',
+                      ),
+                    ),
+                  ),
+                  // Username
+
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 25, vertical: 16),
+                    child: TextField(
+                      controller: _passwordController,
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+                      /// COY EDITS ABOVE CODE
+                      /// AUSTIN EDITS BELOW CODE
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////                        
+
+                        labelText: 'Enter your password',
+                      ),
+                    ),
+                  ),
+
+                  // Password adrgshfjk
+
+
+                  ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        _username = _usernameController.text;
+                        _password = _passwordController.text;
+                      });
+                    },
+                    child: Text('Sign in'),
+                  ),
+                ],
+              ),
+            ),
+          ),
+
+          SizedBox(height: 20),
+          Container(
+            width: 350,
+            padding: EdgeInsets.all(8.0),
+            decoration: BoxDecoration(
+              border: Border.all(
+                width: 1,
+              ),
+              borderRadius: BorderRadius.circular(12),
+            ),
+            child: Column(
+              children: [
+                Text('Username: $_username'),
+                Text('Password: $_password'),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+    var pair = appState.current;
+
+    IconData icon;
+    if (appState.favorites.contains(pair)) {
+      icon = Icons.favorite;
+    } else if (appState.favorites.contains(pair)) {
+      icon = Icons.login_outlined;
+    } else {
+      icon = Icons.favorite_border;
+    }
+
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          BigCard(pair: pair),
+          SizedBox(height: 10),
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ElevatedButton.icon(
+                onPressed: () {
+                  appState.toggleFavorite();
+                },
+                icon: Icon(icon),
+                label: Text('Like'),
+              ),
+              SizedBox(width: 10),
+              ElevatedButton(
+                onPressed: () {
+                  appState.getNext();
+                },
+                child: Text('Next'),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class BigCard extends StatelessWidget {
+  const BigCard({super.key, required this.pair});
+
+  final WordPair pair;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final style = theme.textTheme.displayMedium!.copyWith(
+      color: theme.colorScheme.onPrimary,
+    );
+
+    return Card(
+      color: theme.colorScheme.primary,
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Text(
+          pair.asLowerCase,
+          style: style,
+          semanticsLabel: "${pair.first} ${pair.second}",
+        ),
+      ),
+    );
+  }
+}
+
+class FavoritesPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+    if (appState.favorites.isEmpty) {
+      return Center(child: Text('You have not favorited any words yet.'));
+    }
+
+    return ListView(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(20),
+          child: Text(
+            'You have '
+            '${appState.favorites.length} favorite word pairs:',
+          ),
+        ),
+        for (var pair in appState.favorites)
+          ListTile(
+            leading: Icon(Icons.favorite),
+            title: Text(pair.asLowerCase),
+          ),
+      ],
+    );
+  }
+}
+
+class TestMusic extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+
+          SizedBox(height: 20),
+
+          Container(
+            width: 230,
+            height: 50,
+            decoration: BoxDecoration(
+              border: Border.all(
+                width: 1,
+              ),
+              borderRadius: BorderRadius.circular(12),
+            ),
+            // Main Outside Box
+
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Music Player',
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+            ),
+          ),
+
+          SizedBox(height: 20),
+
+          ListTile(
+            leading: Icon(Icons.music_note),
+            title: Text('Song 1'),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 16),
+            child: ElevatedButton(
+              onPressed: () {
+                
+              },
+              child: Text('Play'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
